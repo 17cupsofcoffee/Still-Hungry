2bcc0bee3d3e27b04426e8514f196f5e6e52edff
Joe Clay <27cupsofcoffee@gmail.com>
2013-08-18 15:11:14 +0100

    Coal blocks can be used in the stove now.

--- src/seventeencups/stillhungry/tileentity/TileStove.java
+++ src/seventeencups/stillhungry/tileentity/TileStove.java
@@ -319,47 +319,43 @@ public class TileStove extends TileEntity implements ISidedInventory {
     }
 
     public static int getItemBurnTime(ItemStack par0ItemStack) {
-        if (par0ItemStack == null) {
+        if (par0ItemStack == null)
+        {
             return 0;
-        } else {
-            int var1 = par0ItemStack.getItem().itemID;
-            Item var2 = par0ItemStack.getItem();
+        }
+        else
+        {
+            int i = par0ItemStack.getItem().itemID;
+            Item item = par0ItemStack.getItem();
 
-            if (par0ItemStack.getItem() instanceof ItemBlock && Block.blocksList[var1] != null) {
-                Block var3 = Block.blocksList[var1];
+            if (par0ItemStack.getItem() instanceof ItemBlock && Block.blocksList[i] != null)
+            {
+                Block block = Block.blocksList[i];
 
-                if (var3 == Block.woodSingleSlab) {
+                if (block == Block.woodSingleSlab)
+                {
                     return 150;
                 }
 
-                if (var3.blockMaterial == Material.wood) {
+                if (block.blockMaterial == Material.wood)
+                {
                     return 300;
                 }
+
+                if (block == Block.field_111034_cE)
+                {
+                    return 16000;
+                }
             }
-            if (var2 instanceof ItemTool && ((ItemTool) var2).getToolMaterialName().equals("WOOD")) {
-                return 200;
-            }
-            if (var2 instanceof ItemSword && ((ItemSword) var2).getToolMaterialName().equals("WOOD")) {
-                return 200;
-            }
-            if (var2 instanceof ItemHoe && ((ItemHoe) var2).getMaterialName().equals("WOOD")) {
-                return 200;
-            }
-            if (var1 == Item.stick.itemID) {
-                return 100;
-            }
-            if (var1 == Item.coal.itemID) {
-                return 1600;
-            }
-            if (var1 == Item.bucketLava.itemID) {
-                return 20000;
-            }
-            if (var1 == Block.sapling.blockID) {
-                return 100;
-            }
-            if (var1 == Item.blazeRod.itemID) {
-                return 2400;
-            }
+
+            if (item instanceof ItemTool && ((ItemTool) item).getToolMaterialName().equals("WOOD")) return 200;
+            if (item instanceof ItemSword && ((ItemSword) item).getToolMaterialName().equals("WOOD")) return 200;
+            if (item instanceof ItemHoe && ((ItemHoe) item).getMaterialName().equals("WOOD")) return 200;
+            if (i == Item.stick.itemID) return 100;
+            if (i == Item.coal.itemID) return 1600;
+            if (i == Item.bucketLava.itemID) return 20000;
+            if (i == Block.sapling.blockID) return 100;
+            if (i == Item.blazeRod.itemID) return 2400;
             return GameRegistry.getFuelValue(par0ItemStack);
         }
     }
